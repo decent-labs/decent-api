885b1820efb949d5989eec7c1727cfcf1f75174b
Adam Gall <adam@decentlabs.io>
2019-10-16 23:09:58 -0400

    Move error handling into controller

--- src/models/note.js
+++ src/models/note.js
@@ -1,10 +1,7 @@
-import { NotFound, InvalidInput } from '../errors';
-
 const table = 'notes';
 
 export const create = (db, note) => {
   return new Promise((resolve, reject) => {
-    if (!note.note) throw new InvalidInput('note')
     return db(table).insert(note).returning('*')
       .then(([note]) => resolve(note))
       .catch(reject);
@@ -13,7 +10,7 @@ export const create = (db, note) => {
 
 export const list = db => {
   return new Promise((resolve, reject) => {
-    return db.from(table).whereNull('deleted_at').select('id', 'note')
+    return db(table).whereNull('deleted_at').select('id', 'note')
       .then(resolve)
       .catch(reject);
   });
@@ -21,34 +18,24 @@ export const list = db => {
 
 export const get = (db, id) => {
   return new Promise((resolve, reject) => {
-    return db.from(table).select('*').whereNull('deleted_at').where({ id }).first()
-      .then(note => {
-        if (!note) throw new NotFound('note');
-        resolve(note);
-      })
+    return db(table).whereNull('deleted_at').where({ id }).select('*').first()
+      .then(resolve)
       .catch(reject);
   });
 };
 
 export const update = (db, note) => {
-  if (!note.note) throw new InvalidInput('note')
   return new Promise((resolve, reject) => {
-    return db(table).whereNull('deleted_at').where({ id: note.id }).update(note).returning('*')
-      .then(([note]) => {
-        if (!note) throw new NotFound('note');
-        return resolve(note);
-      })
+    return db(table).whereNull('deleted_at').where({ id: note.id }).update({ ...note, updated_at: db.fn.now() }).returning('*')
+      .then(([note]) => resolve(note))
       .catch(reject);
   });
 };
 
 export const remove = (db, id) => {
   return new Promise((resolve, reject) => {
-    return db(table).whereNull('deleted_at').where({ id }).update({ deleted_at: db.fn.now() }).returning('*')
-      .then(([note]) => {
-        if (!note) throw new NotFound('note');
-        return resolve(note);
-      })
+    return db(table).whereNull('deleted_at').where({ id }).update({ deleted_at: db.fn.now() }).returning('id')
+      .then(([id]) => resolve(id))
       .catch(reject);
   });
 };
