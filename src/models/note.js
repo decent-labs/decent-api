a55719a45189a8421b25f26a1d599374e3a84717
Adam Gall <adam@decentlabs.io>
2019-10-22 10:15:13 -0400

    Delete all the knex code from note model and use the db parameter to call functions

--- src/models/note.js
+++ src/models/note.js
@@ -1,47 +1,19 @@
-const table = 'notes';
-
-const scrub = note => {
-  if (!note) return null;
-  delete note.deleted_at;
-  return note;
-}
-
 export const create = (db, note) => {
-  return new Promise((resolve, reject) => {
-    return db(table).insert(note).returning('*')
-      .then(([note]) => resolve(scrub(note)))
-      .catch(reject);
-  });
+  return db.createNote(note);
 };
 
 export const list = db => {
-  return new Promise((resolve, reject) => {
-    return db(table).whereNull('deleted_at').select('id', 'note')
-      .then(resolve)
-      .catch(reject);
-  });
+  return db.listNotes();
 };
 
 export const get = (db, id) => {
-  return new Promise((resolve, reject) => {
-    return db(table).whereNull('deleted_at').where({ id }).select('*').first()
-      .then(note => resolve(scrub(note)))
-      .catch(reject);
-  });
+  return db.getNote(id);
 };
 
 export const update = (db, note) => {
-  return new Promise((resolve, reject) => {
-    return db(table).whereNull('deleted_at').where({ id: note.id }).update({ ...note, updated_at: db.fn.now() }).returning('*')
-      .then(([note]) => resolve(scrub(note)))
-      .catch(reject);
-  });
+  return db.updateNote(note);
 };
 
 export const remove = (db, id) => {
-  return new Promise((resolve, reject) => {
-    return db(table).whereNull('deleted_at').where({ id }).update({ deleted_at: db.fn.now() }).returning('id')
-      .then(([id]) => resolve(id))
-      .catch(reject);
-  });
+  return db.deleteNote(id);
 };
