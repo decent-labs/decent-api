cf46c54463c2ed78e9165366762c912041532574
Adam Gall <adam@decentlabs.io>
2019-10-22 13:13:49 -0400

    Use ES6 import style in root test index

--- test/integration/index.js
+++ test/integration/index.js
@@ -7,21 +7,23 @@ import { notes } from './notes';
 
 dotenv.config();
 
-let _testDbManager;
-let _testApi;
+export const integration = () => {
+  let _testDbManager;
+  let _testApi;
 
-describe("integration", function () {
-  before(() => {
-    return databaseSetup(process.env.NODE_ENV).then(db => {
-      _testDbManager = db.manager;
-      _testApi = api(db.database);
+  describe("integration", () => {
+    before(() => {
+      return databaseSetup(process.env.NODE_ENV).then(db => {
+        _testDbManager = db.manager;
+        _testApi = api(db.database);
+      });
+    });
+
+    root(() => _testApi, '/');
+    notes(() => _testApi, '/notes');
+
+    after(() => {
+      return _testDbManager.dropDb().then(() => _testDbManager.close());
     });
   });
-
-  root(() => _testApi, '/');
-  notes(() => _testApi, '/notes');
-
-  after(() => {
-    return _testDbManager.dropDb().then(() => _testDbManager.close());
-  });
-});
+};
