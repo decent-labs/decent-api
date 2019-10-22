47f6abd414189ea41642357442329c1a5e3e4c24
Adam Gall <adam@decentlabs.io>
2019-10-22 13:06:50 -0400

    Update integration tests so that we pass testApi and route into the sub-integration tests

--- test/integration/root.js
+++ test/integration/root.js
@@ -1,51 +1,46 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 
-import { testApi } from '.';
-
 chai.use(chaiHttp);
 chai.should();
 
-describe('/ route tests', () => {
-  let api;
-  const url = '/';
+export const root = (api, route) => {
+  describe(`${route} route tests`, () => {
+    describe(`GET ${route}`, () => {
+      let response;
 
-  before(() => (api = testApi()));
+      before(() => chai.request(api()).get(route).then(res => (response = res)));
 
-  describe('GET /', () => {
-    let response;
+      it('should have 200 status', () => {
+        response.should.have.status(200);
+      });
 
-    before(() => chai.request(api).get(url).then(res => (response = res)));
+      it('should return an object', () => {
+        response.body.should.be.a('object');
+      });
 
-    it('should have 200 status', () => {
-      response.should.have.status(200);
-    });
+      it('should return hello world emojis', () => {
+        response.body.should.have.property('👋').eql('🌎');
+      });
 
-    it('should return an object', () => {
-      response.body.should.be.a('object');
-    });
+      it('should return project name', () => {
+        response.body.should.have.property('name');
+        response.body.name.should.be.a('String');
+      });
 
-    it('should return hello world emojis', () => {
-      response.body.should.have.property('👋').eql('🌎');
-    });
+      it('should return environment', () => {
+        response.body.should.have.property('environment').eql('test');
+      });
 
-    it('should return project name', () => {
-      response.body.should.have.property('name');
-      response.body.name.should.be.a('String');
-    });
+      it('should return version', () => {
+        response.body.should.have.property('version');
+        response.body.version.should.match(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/);
+      });
 
-    it('should return environment', () => {
-      response.body.should.have.property('environment').eql('test');
-    });
-
-    it('should return version', () => {
-      response.body.should.have.property('version');
-      response.body.version.should.match(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/);
-    });
-
-    it('should have a list of endpoints', () => {
-      response.body.should.have.property('endpoints');
-      response.body.endpoints.should.be.a('Array');
+      it('should have a list of endpoints', () => {
+        response.body.should.have.property('endpoints');
+        response.body.endpoints.should.be.a('Array');
+      });
     });
   });
-});
+};
